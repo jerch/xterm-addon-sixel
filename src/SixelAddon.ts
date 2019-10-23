085ad0ea38a89ec581cc34cf48a148814b79ed0e
Jörg Breitbart <jerch@rockborn.de>
2019-10-24 00:46:59 +0200

    update to xterm.js 4.1

--- src/SixelAddon.ts
+++ src/SixelAddon.ts
@@ -6,7 +6,7 @@
  */
 
 import { Terminal, IDisposable } from 'xterm';
-import { SixelImage, toRGBA8888 } from 'sixel';
+import { SixelDecoder, toRGBA8888, DEFAULT_BACKGROUND, PALETTE_ANSI_256 } from 'sixel';
 import { ImageSize, ImageType, ISize as IImageSize } from 'imagesize';
 
 // buffer placeholder
@@ -56,8 +56,8 @@ class ImageStorage {
   private get _cellSize(): CellSize {
     const internalTerm = (this._terminal as any)._core;
     return {
-      width: internalTerm.renderer.dimensions.actualCellWidth,
-      height: internalTerm.renderer.dimensions.actualCellHeight
+      width: internalTerm._renderService.dimensions.actualCellWidth, //term._core._renderService.dimensions
+      height: internalTerm._renderService.dimensions.actualCellHeight
     }
   }
 
@@ -128,6 +128,7 @@ class ImageStorage {
     const buffer = internalTerm.buffer;
     const offset = internalTerm.buffer.x;
 
+    /*
     for (let row = 0; row < rows; ++row) {
       const bufferRow = buffer.lines.get(buffer.y + buffer.ybase);
       for (let col = 0; col < cols; ++col) {
@@ -155,6 +156,34 @@ class ImageStorage {
     } else {
       internalTerm.buffer.x = offset + cols;
     }
+    */
+
+   for (let row = 0; row < rows - 1; ++row) {
+    const bufferRow = buffer.lines.get(buffer.y + buffer.ybase);
+    for (let col = 0; col < cols; ++col) {
+      if (offset + col >= internalTerm.cols) {
+        break;
+      }
+      const tileNum = row * cols + col;
+      bufferRow.setCellFromCodePoint(offset + col, CODE, 1, fg, tileNum);
+    }
+    internalTerm._inputHandler.lineFeed();
+    buffer.x = offset;
+  }
+  // last line
+  const bufferRow = buffer.lines.get(buffer.y + buffer.ybase);
+  for (let col = 0; col < cols; ++col) {
+    if (offset + col >= internalTerm.cols) {
+      break;
+    }
+    const tileNum = (rows - 1) * cols + col;
+    bufferRow.setCellFromCodePoint(offset + col, CODE, 1, fg, tileNum);
+  }
+  buffer.x += cols;
+  if (buffer.x >= internalTerm.cols) {
+    internalTerm._inputHandler.lineFeed();
+  }
+
     return position;
   }
 
@@ -162,7 +191,8 @@ class ImageStorage {
    * Translates a SixelImage into a canvas and calls `addImage`.
    * @param sixel SixelImage
    */
-  public addImageFromSixel(sixel: SixelImage): void {
+  public addImageFromSixel(sixel: SixelDecoder): void {
+    console.log('sixel size', sixel.width, sixel.height);
     const canvas = document.createElement('canvas');
     canvas.width = sixel.width;
     canvas.height = sixel.height;
@@ -172,13 +202,7 @@ class ImageStorage {
 
       // whether current BG should be applied to sixel image
       const applyBG = !!sixel.fillColor;
-      if (applyBG) {
-        // FIXME: get current BG somehow from terminal and convert to RGBA
-        const fill = toRGBA8888(0, 0, 0, 255); // black for now
-        sixel.toImageData(imageData.data, sixel.width, sixel.height, 0, 0, 0, 0, sixel.width, sixel.height, fill);
-      } else {
-        sixel.toImageData(imageData.data, sixel.width, sixel.height);
-      }
+      sixel.toPixelData(imageData.data, sixel.width, sixel.height);
 
       ctx.putImageData(imageData, 0, 0);
       this.addImage(canvas);
@@ -304,8 +328,8 @@ class ImageStorage {
     const internalTerm = (this._terminal as any)._core;
 
     // shamelessly draw on foreign canvas for now
-    // FIXME: needs own layer
-    const ctx: CanvasRenderingContext2D = internalTerm.renderer._renderLayers[0]._ctx;
+    // FIXME: needs own layer term._core._renderService._renderer._renderLayers
+    const ctx: CanvasRenderingContext2D = internalTerm._renderService._renderer._renderLayers[0]._ctx;
 
     this._rescale(imgId);
     const img = this._images[imgId].actual;
@@ -326,41 +350,6 @@ class ImageStorage {
   }
 }
 
-class SIXEL implements IDcsHandler {
-  private _image: SixelImage = new SixelImage();
-
-  constructor(private _store: ImageStorage) {}
-
-  hook(collect: string, params: number[], flag: number): void {
-    /**
-     * We only care for P2 - from the docs:
-     * P2 selects how the terminal draws the background color. You can use one of three values.
-     *    0 or 2 (default) 	Pixel positions specified as 0 are set to the current background color.
-     *    1 	              Pixel positions specified as 0 remain at their current color.
-     * @see: https://vt100.net/docs/vt3xx-gp/chapter14.html
-     */
-
-    // a fill color of 0 indicates to keep empty pixels transparent
-    // any other will apply current BG color in this._store.addImageFromSixel
-    if (params[1] && params[1] === 1) {
-      this._image.fillColor = 0;
-    }
-  }
-
-  put(data: Uint32Array, start: number, end: number): void {
-    this._image.write(data, start, end);
-    // we dont propagate partially transmitted images to the terminal
-  }
-
-  unhook(): void {
-    // propagate the full image to terminal
-    this._store.addImageFromSixel(this._image);
-
-    // reset image to free memory
-    this._image = new SixelImage();
-  }
-}
-
 const FIELD_PARSER: {[key: string]: (data: string) => any} = {
   name: (data: string) => atob(data),
   size: parseInt,
@@ -372,19 +361,23 @@ const FIELD_PARSER: {[key: string]: (data: string) => any} = {
 
 export class SixelAddon implements ITerminalAddon {
   private _imageHandler: IDisposable | null = null;
+  private _sixelHandler: IDisposable | null = null;
   public activate(terminal: Terminal): void {
     const imageStorage = new ImageStorage(terminal);
-
-    // missing terminal.addDcsHandler
-    // thus patch internally for now
-    const _term: any = (terminal as any)._core;
-    (_term._inputHandler as any)._parser.setDcsHandler('q', new SIXEL(imageStorage));
+    
+    // register SIXEL handler
+    this._sixelHandler = terminal.parser.addDcsHandler({final: 'q'}, (data, params) => {
+      const img = new SixelDecoder(params[1] === 1 ? 0 : DEFAULT_BACKGROUND, PALETTE_ANSI_256, 256);
+      img.decodeString(data);
+      imageStorage.addImageFromSixel(img);
+      return true;
+    });
 
     // other image formats
     // use iTerm style for now
     // FIXME in xterm.js - rework osc handler with a DCS handler like interface:
     //  --> explicit hook/unhook, eating chunks of bytes
-    this._imageHandler = terminal.addOscHandler(1337, data => {
+    this._imageHandler = terminal.parser.addOscHandler(1337, data => {
       // skip File=
       const start = (data.startsWith('File=')) ? 5 : 0;
       const divider = data.indexOf(':');
@@ -429,6 +422,10 @@ export class SixelAddon implements ITerminalAddon {
     if (this._imageHandler) {
       this._imageHandler.dispose();
       this._imageHandler = null;
+    }
+    if (this._sixelHandler) {
+      this._sixelHandler.dispose();
+      this._sixelHandler = null;
     }
   }
 }
